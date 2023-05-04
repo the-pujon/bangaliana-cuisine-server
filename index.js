69055f132665f0327689a72b46fee0114fd2b956
Pujon Das Auvi <auvipujondas@gmail.com>
2023-05-04 23:10:49 +0600

    chefs data addded

--- index.js
+++ index.js
@@ -15,27 +15,3 @@ app.get("/", (req, res) => {
 app.get("/chefs", (req, res) => {
   res.send(chefData);
 });
-
-app.get("/chef/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const selectedRecipes = chefData.find((n) => n.id === id);
-
-  res.send(selectedRecipes);
-});
-
-app.get("/recipes", (req, res) => {
-  res.send(recipesData);
-});
-
-app.get("/recipes/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const selectedRecipes = recipesData.filter((n) => n.chef_id === id);
-
-  res.send(selectedRecipes);
-});
-
-app.listen(port, () => {
-  console.log("Your server is running in port: ", port);
-});
